b77ffa1202132dbf9f706fc782427b1ca997e437
Reinger <dvbolotov@edu.hse.ru>
2018-12-02 15:48:55 +0500

    Create class Question and parsing file

---
+++ src/Program.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Program {
+
+    public static void main(String args[]) {
+        ArrayList<Question> questions = new ArrayList<Question>();
+
+        try {
+            FileInputStream fstream = new FileInputStream("Voprosy.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine;
+
+            while ((strLine = br.readLine()) != null) {
+                String[] s = strLine.split("\t");
+                questions.add(new Question(s));
+            }
+        } catch (
+                IOException e) {
+            System.out.println("Ошибка");
+        }
+
+        for (Question q : questions)
+            q.Show();
+    }
+}
